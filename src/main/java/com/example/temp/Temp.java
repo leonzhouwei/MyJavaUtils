50ae3163815f0ae3099dc163a906025a324e41af
zhouwei <leonzhouwei@gmail.com>
2012-12-22 17:46:10 +0800

    add eclipse project files

--- src/main/java/com/example/temp/Temp.java
+++ src/main/java/com/example/temp/Temp.java
@@ -1,8 +1,16 @@
 package main.java.com.example.temp;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
 public class Temp {
 	
 	public static void main(String[] args) {
+		Date date = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		TimeZone tz = TimeZone.getTimeZone("GMT");
+		sdf.setTimeZone(tz);
 		String string = "abc";
 		System.out.println(string.equals(string));
 	}
